2a02d72ab6042da3dd5f6a7b8a436c66f3906a8f
mr-d3falt <79714313+mr-d3falt@users.noreply.github.com>
2022-09-07 12:19:39 +0400

    Update wifi_passwd.bat

--- wifi_passwd.bat
+++ wifi_passwd.bat
@@ -1,7 +1,13 @@
 @echo off
 title WIFI passwd
 echo created by mr-d3falt
-netsh wlan show profile
-echo To check WIFI password, run the following command: 
-echo " netsh wlan show profile name=SSID key=clear | findstr "Key Content" "
+echo .
+echo ################################################
+netsh wlan show profiles | findstr /R /C:"[ ]:[ ]"
+echo ################################################
+echo .
+echo To check WIFI password, type wifi SSID.
+echo .
+set /p SSID="SSID: "
+netsh wlan show profile name="%SSID%" key=clear | findstr "Key Content"
 cmd /k 
